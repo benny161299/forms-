552213225553503c9c3b786c9304096f265acc2b
benny <benny.yesodot@gmail.com>
2026-06-21 14:07:20 +0300

    Corrected config file

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,18 +1,20 @@
 import envSchema from "env-schema";
-const schema = {
-    type: 'object',
-    required: ['MONGO_URI', 'PORT'],
-    properties: {
-        PORT: {
-            type: 'number',
-            default: 3000,
-        },
-        MONGO_URI: {
-            type: 'string',
-        }
-    }
-}
-export const config = envSchema<{ PORT: number; MONGO_URI: string }>({
-  schema: schema,
-  dotenv: true
+import { z } from "zod";
+const schema = z.object({
+  PORT: z.number().default(3000),
+  MONGO_URI: z.string().default("mongodb://localhost:27017/forms")
 });
+
+
+
+export const config = envSchema({
+  schema: z.toJSONSchema(schema, { target: "draft-07" }),
+  dotenv: true
+}); 
+
+
+
+
+
+
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello WorldDDDdd');
 });
 
 app.listen(config.PORT, () => {
