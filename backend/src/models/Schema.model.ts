bda35940df63686784255ef20b6225d24349617b
benny <benny.yesodot@gmail.com>
2026-06-30 16:16:52 +0300

    add QuestionMongooseSchema

--- backend/src/models/Schema.model.ts
+++ backend/src/models/Schema.model.ts
@@ -1,2 +1,13 @@
 import mongoose, { Schema } from 'mongoose';
 import { Ischema, ISection, IQuestion } from '../types/schema.types.js';
+
+const QuestionMongooseSchema = new Schema<IQuestion>({
+  id: { type: String, required: true },    
+  title: { type: String, required: true },
+  type: { 
+    type: String, 
+    required: true 
+  },
+  required: { type: Boolean, required: true },
+  options: { type: Schema.Types.Mixed }       
+}, { _id: false });
