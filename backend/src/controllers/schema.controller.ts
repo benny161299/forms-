4e8df18f4c0e498438b1925b08eb263338e0dbc5
benny <benny.yesodot@gmail.com>
2026-07-05 16:07:55 +0300

    add getSchemasDrafts controller function

--- backend/src/controllers/schema.controller.ts
+++ backend/src/controllers/schema.controller.ts
@@ -23,3 +23,14 @@ export const getSchemas = async (req: Request, res: Response) => {
     return res.status(500).json({ error: 'Internal server error' });
   }
 };
+
+
+export const getSchemaDrafts = async (req: Request, res: Response) => {
+  try {
+    const drafts = await SchemaModel.find({ isDraft: true });
+    return res.status(200).json(drafts);
+  } catch (error) {
+    console.error('Error fetching schemas:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
