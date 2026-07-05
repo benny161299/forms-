af4f158d3372608bf58ffae17a303f8297425d34
benny <benny.yesodot@gmail.com>
2026-07-05 09:58:49 +0300

    add db connection function in server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import { config } from './config/config.js'
+import { connectDB } from './config/db.js';
+
 const app = express();
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello WorldDDDddddd');
+  res.send('Hello World');
 });
+
+try {
+  await connectDB();
+  app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+  });
+} catch (error) {
+  console.error('Failed to start server:', error);
+  process.exit(1);
+}
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on port ${config.PORT}`);
