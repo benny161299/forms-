2b0eb3ec53cf545299e6d30b1ebce67b37765881
benny <benny.yesodot@gmail.com>
2026-07-07 11:21:31 +0300

    add post to the instance.routes

---
+++ backend/src/routes/instance.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { z } from 'zod';
+import { validateRequest } from '../middlewares/validation.middleware.js';
+import { instanceSchema } from '../types/instance.types.js';
+import {
+  createInstance,
+  getInstances,
+  getInstancesDrafts, 
+  getInstancesBySchemaId,
+  getInstanceById,
+  updateInstance,
+  deleteInstance,
+} from '../controllers/instance.controller.js';
+
+const router = Router();
+
+const emptyObj = z.object({});
+
+const idSchema = z.object({
+  id: z.string().regex(/^[0-9a-fA-F]{24}$/)
+});
+
+
+router.post(
+  '/',
+  validateRequest({
+    body: instanceSchema,
+    query: emptyObj,
+    params: emptyObj
+  }),
+  createInstance
+);
