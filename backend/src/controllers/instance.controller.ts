c622d10f0a3a6a809c890ed2027a60035be3c6a4
benny <benny.yesodot@gmail.com>
2026-07-07 10:28:27 +0300

    update getInstancesBySchemaId controller function

--- backend/src/controllers/instance.controller.ts
+++ backend/src/controllers/instance.controller.ts
@@ -36,8 +36,11 @@ export const getInstancesDrafts = async (req: Request, res: Response) => {
 export const getInstancesBySchemaId = async (req: Request, res: Response) => {
   try {
     const { schemaId } = req.params;
-    const instances = await InstanceModel.find({ schemaId});
-    return res.status(200).json(instances);
+    const instance = await InstanceModel.find({schemaId});
+    if (!instance) {
+      return res.status(404).json({ error: 'Instance not found' });
+    }
+    return res.status(200).json(instance);
   } catch (error) {
     console.error('Error fetching instances by schemaId:', error);
     return res.status(500).json({ error: 'Internal server error' });
