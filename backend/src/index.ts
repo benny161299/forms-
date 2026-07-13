66b437e1706ff5e94acdb40baf9616309b0ae152
benny <benny.yesodot@gmail.com>
2026-07-13 15:34:39 +0300

    update error Middleware in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,13 @@ import { config } from './config/config.js'
 import { connectDB } from './config/db.js';
 import schemaRoutes from './services/schemas/schema.routes.js';
 import instanceRoutes from './services/instances/instance.routes.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 const app = express();
 
 app.use(express.json());
 app.use('/api/schemas', schemaRoutes);
 app.use('/api/instances', instanceRoutes);
-
+app.use(errorMiddleware);
 async function startServer() {
   try {
     await connectDB();
