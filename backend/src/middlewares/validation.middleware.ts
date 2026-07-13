3311eb01ae1c0318b22a640952f47fef9eb530a7
benny <benny.yesodot@gmail.com>
2026-07-13 13:34:54 +0300

    update validation middleware

--- backend/src/middlewares/validation.middleware.ts
+++ backend/src/middlewares/validation.middleware.ts
@@ -1,37 +1,57 @@
 import { Request, Response, NextFunction } from 'express';
 import { ZodType, z } from 'zod';
+import { StatusCodes } from 'http-status-codes';
+import { AppError } from './error.middleware.js';
 
 interface RequestValidationSchemas {
-    body?: ZodType;
-    query?: ZodType;
-    params?: ZodType;
+  body?: ZodType;
+  query?: ZodType;
+  params?: ZodType;
 }
 
 export const validateRequest = (schemas: RequestValidationSchemas) => {
-    return (req: Request, res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
 
-        if (schemas.params) {
-            const result = schemas.params.safeParse(req.params);
-            if (!result.success) {
-                return res.status(400).json({ status: 'error', message: 'Invalid Params', errors: z.treeifyError(result.error) });
-            }
-        }
+    if (schemas.params) {
+      const result = schemas.params.safeParse(req.params);
+      if (!result.success) {
+        return next(
+          new AppError(
+            'Invalid Params',
+            StatusCodes.BAD_REQUEST,
+            z.treeifyError(result.error)
+          )
+        );
+      }
+    }
 
-        if (schemas.query) {
-            const result = schemas.query.safeParse(req.query);
-            if (!result.success) {
-                return res.status(400).json({ status: 'error', message: 'Invalid Query', errors: z.treeifyError(result.error) });
-            }
-        }
+    if (schemas.query) {
+      const result = schemas.query.safeParse(req.query);
+      if (!result.success) {
+        return next(
+          new AppError(
+            'Invalid Query',
+            StatusCodes.BAD_REQUEST,
+            z.treeifyError(result.error)
+          )
+        );
+      }
+    }
 
-        if (schemas.body) {
-            const result = schemas.body.safeParse(req.body);
-            if (!result.success) {
-                return res.status(400).json({ status: 'error', message: 'Invalid Body', errors: z.treeifyError(result.error) });
-            }
-            req.body = result.data;
-        }
+    if (schemas.body) {
+      const result = schemas.body.safeParse(req.body);
+      if (!result.success) {
+        return next(
+          new AppError(
+            'Invalid Body',
+            StatusCodes.BAD_REQUEST,
+            z.treeifyError(result.error)
+          )
+        );
+      }
+      req.body = result.data;
+    }
 
-        return next();
-    };
+    return next();
+  };
 };
