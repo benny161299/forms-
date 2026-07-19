2adc52194c55a87900c38843b801687ecada5a33
benny <benny.yesodot@gmail.com>
2026-07-19 11:22:13 +0300

    update and change schema.manager

--- backend/src/services/schemas/schema.manager.ts
+++ backend/src/services/schemas/schema.manager.ts
@@ -4,40 +4,29 @@ import { StatusCodes } from 'http-status-codes';
 import { Ischema } from './schema.types.js'; 
 
 export const createNewSchema = async (schemaData: Ischema) => {
-  return await SchemaModel.create(schemaData);
+  return SchemaModel.create(schemaData);
 };
 
 export const fetchAllSchemas = async () => {
-  return await SchemaModel.find({ isDraft: false });
+  return SchemaModel.find({ isDraft: false });
 };
 
 export const fetchAllSchemaDrafts = async () => {
-  return await SchemaModel.find({ isDraft: true });
+  return SchemaModel.find({ isDraft: true });
 };
 export const fetchSchemaById = async (id: string) => {
-  const schema = await SchemaModel.findById(id);
-  if (!schema) {
-    throw new AppError('Schema not found', StatusCodes.NOT_FOUND);
-  }
-  return schema;
+  return SchemaModel.findById(id)
+    .orFail(() => new AppError('Schema not found', StatusCodes.NOT_FOUND));
 };
 
 export const updateSchemaManager = async (id: string, updateData: Ischema) => {
-  const updatedSchema = await SchemaModel.findOneAndUpdate(
+  return SchemaModel.findOneAndUpdate(
     { _id: id, isDraft: true },
     updateData
-  );
-
-  if (!updatedSchema) {
-    throw new AppError('Schema not found, or it is not a draft', StatusCodes.NOT_FOUND);
-  }
-
-  return updatedSchema;
+  ).orFail(() => new AppError('Schema not found, or it is not a draft', StatusCodes.NOT_FOUND));
 };
 
 export const removeSchema = async (id: string) => {
-  const deletedSchema = await SchemaModel.findByIdAndDelete(id);
-  if (!deletedSchema) {
-    throw new AppError('Schema not found', StatusCodes.NOT_FOUND);
-  }
+  return SchemaModel.findByIdAndDelete(id)
+    .orFail(() => new AppError('Schema not found', StatusCodes.NOT_FOUND));
 };
