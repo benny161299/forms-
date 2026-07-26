3d8e8ad6aa62bcbfe826758af0f375d4d47f52ca
benny <benny.yesodot@gmail.com>
2026-07-26 15:12:01 +0300

    add v4 to z.record(z.uuid()

--- backend/src/services/instances/instance.types.ts
+++ backend/src/services/instances/instance.types.ts
@@ -15,7 +15,7 @@ export const instanceSchema = z.object({
   _id: mongoId.optional(),
   schemaId: mongoId,
   isDraft: z.boolean(),
-  answers: z.record(z.uuid(), answerValueSchema),
+  answers: z.record(z.uuidv4(), answerValueSchema),
 });
 
 export type IInstance = z.infer<typeof instanceSchema>;
