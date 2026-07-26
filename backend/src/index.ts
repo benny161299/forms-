b2660542ab06c6aec10d1c165271714c3e6e5203
benny <benny.yesodot@gmail.com>
2026-07-26 13:37:13 +0300

    move the  connectDB above the try

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,9 @@ const app = express();
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use(errorMiddleware);
+await connectDB();
 try {
-  await connectDB();
+  
   app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
   });
