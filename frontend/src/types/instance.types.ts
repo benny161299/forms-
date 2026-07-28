726007d483a00edf40817dcd0cf10581e78c8a1c
benny <benny.yesodot@gmail.com>
2026-07-28 10:56:13 +0300

    create frontend folder structure

--- frontend/src/types/instance.types.ts
+++ frontend/src/types/instance.types.ts
@@ -1,21 +1,23 @@
-import { z } from 'zod';
+import { z } from "zod";
 
+const mongoId = z.string().regex(/^[0-9a-fA-F]{24}$/);
 export const answerValueSchema = z.union([
   z.string(),
   z.number(),
   z.array(z.string()),
   z.record(z.string(), z.number()),
-  z.record(z.string(), z.array(z.number()))
+  z.record(z.string(), z.array(z.number())),
 ]);
 
 export type AnswerValue = z.infer<typeof answerValueSchema>;
 
-
 export const instanceSchema = z.object({
-  _id: z.string(),
-  schemaId: z.string(),
+  _id: mongoId.optional(),
+  schemaId: mongoId,
   isDraft: z.boolean(),
-  answers: z.record(z.string(), answerValueSchema)
+  answers: z.record(z.uuidv4(), answerValueSchema),
 });
 
 export type IInstance = z.infer<typeof instanceSchema>;
+export type CreateInstanceInput = Pick<IInstance, "schemaId">;
+export type UpdateInstanceInput = Pick<IInstance, "answers">;
